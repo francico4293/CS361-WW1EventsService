b8a2bab09d34ddd4f57647d7c10ce0a77aa45934
Colin Francis <colinfrancis@Colins-MacBook-Pro.local>
2022-10-26 17:31:32 -0400

    July August September response testing - 500 for July found

--- tests/response_tests.py
+++ tests/response_tests.py
@@ -13,6 +13,9 @@ class TestWW1EventsApi(unittest.TestCase):
     __APRIL = 4
     __MAY = 5
     __JUNE = 6
+    __JULY = 7
+    __AUGUST = 8
+    __SEPTEMBER = 9
     __DECEMBER = 12
     __DAYS_IN_JANUARY = 31
     __DAYS_IN_FEBRUARY = 28
@@ -20,6 +23,9 @@ class TestWW1EventsApi(unittest.TestCase):
     __DAYS_IN_APRIL = 30
     __DAYS_IN_MAY = 31
     __DAYS_IN_JUNE = 30
+    __DAYS_IN_JULY = 31
+    __DAYS_IN_AUGUST = 31
+    __DAYS_IN_SEPTEMBER = 30 
     __DAYS_IN_DECEMBER = 31
     
     def test_api_response_january(self):
@@ -55,6 +61,24 @@ class TestWW1EventsApi(unittest.TestCase):
     def test_api_response_june(self):
         for day in range(1, self.__DAYS_IN_JUNE + 1):
             data = { self.__DAY: day, self.__MONTH: self.__JUNE }
+            response = requests.get(self.__URL, json=data)
+            self.assertEqual(response.status_code, 200)
+    
+    def test_api_response_july(self):
+        for day in range(1, self.__DAYS_IN_JULY + 1):
+            data = { self.__DAY: day, self.__MONTH: self.__JULY }
+            response = requests.get(self.__URL, json=data)
+            self.assertEqual(response.status_code, 200)
+    
+    def test_api_response_august(self):
+        for day in range(1, self.__DAYS_IN_AUGUST + 1):
+            data = { self.__DAY: day, self.__MONTH: self.__AUGUST }
+            response = requests.get(self.__URL, json=data)
+            self.assertEqual(response.status_code, 200)
+    
+    def test_api_response_september(self):
+        for day in range(1, self.__DAYS_IN_SEPTEMBER + 1):
+            data = { self.__DAY: day, self.__MONTH: self.__SEPTEMBER }
             response = requests.get(self.__URL, json=data)
             self.assertEqual(response.status_code, 200)
     
